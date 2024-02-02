b5a95f5ee3f65bfefc6bdb83c2d4efea13262f7a
Eason <s7887177@gmail.com>
2024-02-02 17:58:19 +0800

    c

--- Assets/Scripts/EasonOdinEditor/DateTimeAttributeDrawer.cs
+++ Assets/Scripts/EasonOdinEditor/DateTimeAttributeDrawer.cs
@@ -2,31 +2,69 @@ using Sirenix.OdinInspector.Editor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 namespace Eason.Odin.Editor
 {
 
 
      public class DateTimeAttributeDrawer : OdinAttributeDrawer<DateTimeAttribute>
     {
-        private string tempString;
+        private string _tempString;
+        private string _id;
+        private bool _focused;
+        private bool _justFocused;
+        private bool _justLostFocus;
+        protected override void Initialize()
+        {
+            base.Initialize();
+            _tempString = new DateTime((long)Property.ValueEntry.WeakSmartValue).ToString(Attribute.format);
+            _id = Guid.NewGuid().ToString();
+            Debug.Log(_id);
+        }
         protected override void DrawPropertyLayout(GUIContent label)
         {
-            if(Property.ParentType != typeof(long))
+            if(Property.ValueEntry.TypeOfValue != typeof(Int64))
             {
-                
                 base.DrawPropertyLayout(label);
-                var  input = GUI.TextField(Property.LastDrawnValueRect, new DateTime((long)Property.ValueEntry.WeakSmartValue).ToString(Attribute.format));
-                try
-                {
-                    var dateTime = DateTime.Parse(input);
-                    Property.ValueEntry.WeakSmartValue = dateTime.Ticks;
-                }
-                catch { }
-
                 return;
             }
+            GUI.SetNextControlName(_id);
+            if(label == null)
+            {
+                _tempString = GUILayout.TextField(_tempString);
+            }
+            else
+            {
+                _tempString = EditorGUILayout.TextField(label, _tempString);
+            }
 
+            if (GUI.GetNameOfFocusedControl() == _id)
+            {
+                if (!_focused) _justFocused = true;
+                _focused = true;
+            }
+            else
+            {
+                if (_focused) _justLostFocus = true;
+                _focused = false;
+            }
+            if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return ) { _justLostFocus = true; }
+
+            if (_justLostFocus)
+            {
+                try
+                {
+                    Property.ValueEntry.WeakSmartValue = DateTime.Parse(_tempString).Ticks; 
+                }
+                catch (Exception e){
+                    Debug.LogException(e);
+                }
+
+                _tempString = new DateTime((long)Property.ValueEntry.WeakSmartValue).ToString(Attribute.format);
+            }
+            _justFocused = _justLostFocus = false;
         }
     }
 }
--- Assets/Scripts/Smart360/VerificationSystem.cs
+++ Assets/Scripts/Smart360/VerificationSystem.cs
@@ -1,7 +1,9 @@
 ﻿using Eason.Odin;
 using Sirenix.OdinInspector;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 
@@ -9,44 +11,40 @@ using UnityEngine;
 [Serializable]
 public struct ModuleVerificationInfo
 {
-    public static byte[] GOLDEN_FINGER_HASH => new byte[10] {0,1,2,3,4,5,6,7,8,9 };
-
     [SerializeField] private int _id;
-    [SerializeField, HideInInspector] private long _expiredDate;
-    [SerializeField] private string _hash;
-    [ShowInInspector] UDateTime _time
-    {
-        get => new DateTime(_expiredDate);
-        set => _expiredDate = value.dateTime.Ticks;
-    }
-    private byte[] GetHash()
-    {
-        return GOLDEN_FINGER_HASH;
-    }
+    [SerializeField, DateTime] private long _expiredDate;
+    [SerializeField,HideInInspector] private string _hash;
+
+    public int id { get => _id; internal set => _id = value; }
+    public long expiredDate { get => _expiredDate; }
+    public string Hash { get => _hash; internal set => _hash = value; }
 }
 [Serializable]
 public struct EditionVerificationInfo
 {
-    public static byte[] GOLDEN_FINGER_HASH => new byte[10] { 9,8,7,6,5,4,3,2,1,0 };
-
     [SerializeField] private int _id;
-    [SerializeField] private long _expiredDate;
-    [SerializeField] private string _hash;
-    private byte[] GetHash()
-    {
-        return GOLDEN_FINGER_HASH;
-    }
+    [SerializeField, DateTime] private long _expiredDate;
+    [SerializeField, HideInInspector] private string _hash;
+
+    public int id { get => _id; internal set => _id = value; }
+    public long expiredDate { get => _expiredDate; }
+    public string Hash { get => _hash; internal set => _hash = value; }
 }
 
 [Serializable]
 public struct VerificationInfo
 {
-    [SerializeField] private long _applicationExpiredDate;
-    [SerializeField] private ModuleVerificationInfo[] _modules;
-    [SerializeField] private EditionVerificationInfo[] _editions;
-
-
+    [SerializeField, DateTime] private long _applicationExpiredDate;
+    [SerializeField] private string _serialNumber;
+    [SerializeField, TableList] private ModuleVerificationInfo[] _modules;
+    [SerializeField, TableList] private EditionVerificationInfo[] _editions;
     [SerializeField] private string _hash;
+
+    public long applicationExpiredDate { get => _applicationExpiredDate; }
+    public string serialNumber { get => _serialNumber; }
+    public ModuleVerificationInfo[] modules { get => _modules.ToArray(); }
+    public EditionVerificationInfo[] editions { get => _editions.ToArray(); }
+    public string hash { get => _hash; set => _hash = value; }
 }
 
 [Serializable]
@@ -72,10 +70,7 @@ public class VerificationSystem : MonoBehaviour
     [Header("States")]
     [NonSerialized, ShowInInspector, ReadOnly] private bool _initialized;
     [SerializeField] private VerificationInfo _verificationInfo;
-    [NonSerialized, ShowInInspector, ReadOnly] ModuleVerificationInfo[] _modules;
-    [NonSerialized, ShowInInspector, ReadOnly] EditionVerificationInfo[] _editions;
-    //[Header("Debug")]
-    [DateTime, SerializeField] private long _dateTime;
+
 
 
     public bool VerifyPolicyViolation()
@@ -86,17 +81,60 @@ public class VerificationSystem : MonoBehaviour
     {
         if(_initialized) { throw new Exception("This should not happen. Check the error immdiately!"); }
         
-        LoadVerificationResult();
+        LoadVerificationInfo();
+    }
+    [Button]
+    private string HashVerificationInfo()
+    {
+        return HashVerificationInfo(_verificationInfo);
+    }
+
+    private string HashVerificationInfo(VerificationInfo info)
+    {
+        var numList = new Queue<long>();
+        numList.Enqueue(info.applicationExpiredDate);
+        numList.Enqueue(info.serialNumber.GetHashCode());
+        for (int i = 0; i < info.modules.Length; i++)
+        {
+            numList.Enqueue(info.modules[i].id);
+            numList.Enqueue(info.modules[i].expiredDate);
+        }
+        for (int i = 0; i < info.editions.Length; i++)
+        {
+            numList.Enqueue(info.editions[i].id);
+            numList.Enqueue(info.editions[i].expiredDate);
+        }
+        long num = numList.Dequeue();
+        while (numList.Any())
+        {
+            var power = numList.Dequeue();
+            if (power == 0) continue;
+            num = Int64Pow(num, power);
+        }
+        return num.ToString();
+    }
+
+    Int64 Int64Pow(Int64 x, Int64 pow)
+    {
+        Int64 ret = 1;
+        while (pow != 0)
+        {
+            if ((pow & 1) == 1)
+                ret *= x;
+            x *= x;
+            pow >>= 1;
+        }
+        return ret;
     }
 
     [Button, HideIf(nameof(folderNotExist))]
-    private void SaveVerificationResult()
+    private void SaveVerificationInfo()
     {
         var json = JsonUtility.ToJson(_verificationInfo);
         File.WriteAllText(Path.Combine(Application.persistentDataPath, _verificationFolderPath, _resultFileName), json);
     }
     [Button]
-    private void LoadVerificationResult()
+    private void LoadVerificationInfo()
     {
         var json = File.ReadAllText(Path.Combine(Application.persistentDataPath, _verificationFolderPath, _resultFileName));
         _verificationInfo = JsonUtility.FromJson<VerificationInfo>(json);
