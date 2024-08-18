3c9b93c1eddd78fab455edf04d70813258c40638
Eason <s7887177@gmail.com>
2024-08-18 13:48:21 +0800

    c

---
+++ Assets/Scripts/Editor/EnableFileSharingPostBuildProcessor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEditor;
+using System.IO;
+
+public class EnableFileSharingPostBuildProcessor
+{
+    [PostProcessBuild]
+    public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget == BuildTarget.iOS)
+        {
+            // Get plist
+            string plistPath = pathToBuiltProject + "/Info.plist";
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            // Get root
+            PlistElementDict rootDict = plist.root;
+
+            rootDict.SetBoolean("UIFileSharingEnabled", true);
+
+            // Write to file
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
+    }
+}
